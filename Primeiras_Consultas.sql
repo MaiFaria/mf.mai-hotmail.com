53a498b36c134e119c41db2a1ef8ab15ba71f213
unknown <mf.mai@hotmail.com>
2020-03-31 21:53:02 -0300

    Adicionando novas consultas praticando o INNER JOIN e SUB-CONSULTAS

--- Primeiras_Consultas.sql
+++ Primeiras_Consultas.sql
@@ -24,4 +24,22 @@ SELECT nome,
          ELSE 'Crianças' END
          FROM tabela_de_clientes;
 
-         
+--Levando em considedração que o valor financeiro das vendas consiste em multiplicar a quantidade pelo preço, 
+--temos a seguir o faturammento anual.
+SELECT TO_CHAR(data_venda, 'YYYY'), SUM (quantidade * preco) AS Faturamento
+FROM notas_fiscais NF INNER JOIN itens_notas_fiscais INF
+ON nf.numero = inf.numero
+GROUP BY TO_CHAR(data_venda, 'YYYY');
+
+--Tendo em vista esta consulta:
+SELECT cpf, COUNT(*)
+FROM notas_fiscais
+WHERE TO_CHAR(data_venda, 'YYYY') = '2016'
+GROUP BY cpf
+HAVING COUNT (*) > 2000;
+--Temos a mesma em Sub-Consulta
+SELECT X.cpf, X.contador
+FROM(SELECT cpf, COUNT(*) AS contador FROM notas_fiscais WHERE TO_CHAR(data_venda, 'YYYY') = '2016' GROUP BY cpf) X
+WHERE X.contador > 2000;
+
+
